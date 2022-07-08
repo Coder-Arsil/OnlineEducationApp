15f41a042e827d75879e9947c3b2abbb2843169c
Mohammad Arsil <arsil8356@gmail.com>
2022-07-08 11:54:44 +0530

    commit

--- MySchool/src/screens/UserData.js
+++ MySchool/src/screens/UserData.js
@@ -1,4 +1,4 @@
-import { FlatList, StyleSheet, Text, View } from 'react-native'
+import { FlatList, Image, StyleSheet, Text, View } from 'react-native'
 import React, { useEffect, useState } from 'react'
 
 const UserData = () => {
@@ -8,29 +8,63 @@ const UserData = () => {
             const response = await fetch("https://thapatechnical.github.io/userapi/users.json");
             const realData = response.json()
             setData(realData)
-            console.log(Mydata)
+            // console.log(Mydata)
         }
-        catch(error) {
+        catch (error) {
             console.log(error)
         }
     }
-    useEffect(() => {
+    useEffect(() =>getUserData() ,[])
+    const showUserData = ({ item }) => {
         return (
-            getUserData()
+            <>
+                <View style={styles.card}>
+                    <View style={styles.imgContainer}>
+                        <Image style={styles.imgStyle}
+                            source={{ uri: item.image }}
+                        />
+                    </View>
+                    <View>
+                        <View style={styles.bioDataContainer}>
+                            <Text style={styles.bioData}>Bio-Data</Text>
+                            <Text style={styles.idNumber}>
+                                {item.id < 10 ? `#0${item.id}` : `#0{item.id}`}
+                            </Text>
+                        </View>
+                        <View style={styles.mainContent}>
+                            <Text style={styles.myName}>
+                                Name : {item.name}
+                            </Text>
+                            <Text style={styles.email}>
+                                Email : {item.name}
+                            </Text>
+                            <Text style={styles.mobile}>
+                                Mobile : {item.name}
+                            </Text>
+                        </View>
+                    </View>
+                </View>
+            </>
         )
-    }, [])
+    }
     return (
         <View>
             <Text>List of Students</Text>
             <FlatList
-                keyExtractor={(item)=>item}
                 data={Mydata}
-                renderItem={ ()=>{}}
+                renderItem={showUserData}
             />
         </View>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    card: {
+        width: "100%",
+        height: "100%",
+        paddingVertical: 50,
+        backgroundColor: "gray"
+    }
+})
 
 export default UserData
